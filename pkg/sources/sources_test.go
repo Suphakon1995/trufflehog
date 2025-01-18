cd33c695620c3d8096b7387e0b917b58fd0ce697
Suphakon1995 <195579934+Suphakon1995@users.noreply.github.com>
2025-01-19 04:19:08 +0700

    Update sources_test.go

--- pkg/sources/sources_test.go
+++ pkg/sources/sources_test.go
@@ -1,14 +1,1 @@
-package sources
 
-import (
-	"testing"
-	"unsafe"
-
-	"github.com/stretchr/testify/assert"
-)
-
-// TestChunkSize ensures that the Chunk struct does not exceed 80 bytes.
-func TestChunkSize(t *testing.T) {
-	t.Parallel()
-	assert.Equal(t, unsafe.Sizeof(Chunk{}), uintptr(80), "Chunk struct size exceeds 80 bytes")
-}
