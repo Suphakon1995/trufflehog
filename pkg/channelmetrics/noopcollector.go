3fa73c26f199d0fc2e8aef49bed49bfdbac12414
Suphakon1995 <195579934+Suphakon1995@users.noreply.github.com>
2025-01-19 12:08:42 +0700

    Update noopcollector.go

--- pkg/channelmetrics/noopcollector.go
+++ pkg/channelmetrics/noopcollector.go
@@ -1,12 +1,1 @@
-package channelmetrics
 
-import "time"
-
-// noopCollector is a default implementation of the MetricsCollector interface
-// for internal package use only.
-type noopCollector struct{}
-
-func (noopCollector) RecordProduceDuration(duration time.Duration) {}
-func (noopCollector) RecordConsumeDuration(duration time.Duration) {}
-func (noopCollector) RecordChannelLen(size int)                    {}
-func (noopCollector) RecordChannelCap(capacity int)                {}
